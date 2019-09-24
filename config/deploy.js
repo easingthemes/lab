7659798688dc6604fe2af60b36eec679725a190e
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2019-09-24 22:42:59 +0200

    [ci] logging update

--- config/deploy.js
+++ config/deploy.js
@@ -6,6 +6,9 @@ const trunk = REPO_URL.replace('.git', '/trunk/');
 const url = PUBLIC_URL + '/' + URL + '?token=' + TOKEN + '&repo=' + trunk + DIST_DIR;
 
 https.get(url, (resp) => {
+    console.log('statusCode:', resp.statusCode);
+    console.log('headers:', resp.headers);
+
     let data = '';
 
     resp.on('data', (chunk) => {
@@ -13,7 +16,7 @@ https.get(url, (resp) => {
     });
 
     resp.on('end', () => {
-        console.log(JSON.parse(data).explanation);
+        console.log(data);
     });
 
 }).on("error", (err) => {
